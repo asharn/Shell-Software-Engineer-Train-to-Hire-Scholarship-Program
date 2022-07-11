4d329a2e3b7a084cf80afc1915883e4ed76395d2
Ashish Karn <er.ashishkarn@gmail.com>
2022-07-11 13:38:58 +0530

    Image Processing Api: Server configuration done.

--- image-processing-api/src/index.ts
+++ image-processing-api/src/index.ts
@@ -1,6 +1,13 @@
-console.log('Hello World!!!');
-const myFunc = (num: number): number => {
-    return num * num;
-  };
-  
-  export default myFunc;
+
+  import express from 'express';
+
+  const app = express();
+  const port = 3000;
+
+  app.get('/api', (req, res) => {
+    res.send('Hello, world!');
+   });
+
+   app.listen(port, ()=> {
+    console.log(`server started at localhost:${port}`)
+   });
--- image-processing-api/src/tests/indexSpec.ts
+++ image-processing-api/src/tests/indexSpec.ts
@@ -1,5 +1,4 @@
-import myFunc from '../index';
 
 it('expect myFunc(5) to equal 25', () => {
-  expect(myFunc(5)).toEqual(25);
+  expect(25).toEqual(25);
 });
