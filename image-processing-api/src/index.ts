333b8c43eafb6640dbe845300cae38c8c741fd70
Ashish Karn <er.ashishkarn@gmail.com>
2022-07-11 13:56:34 +0530

    Image Processing Api: Morgan middleware configure for logging purpose.

--- image-processing-api/src/index.ts
+++ image-processing-api/src/index.ts
@@ -1,13 +1,25 @@
 
   import express from 'express';
+  const morgan = require("morgan")
 
   const app = express();
+
+  // Middlewares
+  app.use(morgan("common"))
+
+  // Port
   const port = 3000;
 
-  app.get('/api', (req, res) => {
-    res.send('Hello, world!');
+  app.get("/", (req, res) => {
+    res.json({
+      message: "Hello Stranger! How are you?",
+    });
+  });
+
+  app.get('/route', (req, res) => {
+    res.send('Hello, You called, api route!');
    });
 
    app.listen(port, ()=> {
-    console.log(`server started at localhost:${port}`)
+    console.log(`Server started at localhost:${port}`)
    });
