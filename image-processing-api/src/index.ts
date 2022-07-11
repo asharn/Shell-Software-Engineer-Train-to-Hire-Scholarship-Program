14e503fc7dab4dc77d0dce010356225cb6d0fc71
Ashish Karn <er.ashishkarn@gmail.com>
2022-07-11 14:09:19 +0530

    Image Processing Api: Rate Limit middleware configure to restrict hitting of api 5 within a minute.

--- image-processing-api/src/index.ts
+++ image-processing-api/src/index.ts
@@ -3,16 +3,26 @@
   const morgan = require("morgan");
   const helmet = require("helmet");
   const cors = require("cors");
+  const rateLimit = require("express-rate-limit");
 
   const app = express();
+
+    //Rate limit for each API'a
+    const limiter = rateLimit({
+        windowMs: 60 * 1000, // 1 minute
+        max: 5, // limit each IP to 2 requests per windowMs
+        message: "Too many request created from this IP, please try again after a minute"
+      });
 
   // Middlewares
   app.use(morgan("common"));
   app.use(helmet());
   app.use(cors());
+  app.use(limiter); //  apply to all requests
 
   // Port
   const port = 3000;
+
 
   app.get("/", (req, res) => {
     res.json({
