4db80fd25f242e9c213c5130797e68fffbc5b19e
Ashish Karn <er.ashishkarn@gmail.com>
2022-07-18 20:55:03 +0530

    React-Redux-Bindings: Goals-Todos app created and UI mapped accordingly and app is running correctly

--- exercise-on-goals-todos-app/src/index.js
+++ exercise-on-goals-todos-app/src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/app/App";
+import App from "./components/App/App";
+import reducer from "./reducers";
+import middleware from "./middleware";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const store = createStore(reducer, middleware);
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
---
+++ exercise-on-goals-todos-app/src/utilities/generate.js
@@ -0,0 +1,6 @@
+
+
+
+export default function generateId() {
+    return Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36);
+  }
