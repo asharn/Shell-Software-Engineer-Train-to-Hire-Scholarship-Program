46510655fa6873c1543c4d1992f361dc3bb56ade
Ashish Karn <er.ashishkarn@gmail.com>
2022-08-14 13:27:21 +0530

    Near Earth Objects - Task 3b: Query the database of close approaches using user-specified criteria completed

--- near-earth-objects/database.py
+++ near-earth-objects/database.py
@@ -105,4 +105,5 @@ class NEODatabase:
         """
         # TODO: Generate `CloseApproach` objects that match all of the filters.
         for approach in self._approaches:
-            yield approach
+            if all(filter(approach) for filter in filters):
+                yield approach
