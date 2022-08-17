531e72249f09561da8684e2ec191cde14ef75636
Ashish Karn <er.ashishkarn@gmail.com>
2022-08-17 16:55:55 +0530

    Azure Practice - Connect app to Store issue got resolved.

--- practice-azure/connect-app-to-storage-starter/config.py
+++ practice-azure/connect-app-to-storage-starter/config.py
@@ -8,7 +8,7 @@ class Config(object):
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'hello-world-db'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'azureadmin'
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'game@123over'
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+18+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'helloworldstorageacc'
