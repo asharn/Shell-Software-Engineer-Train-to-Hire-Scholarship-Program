fef29b0af328f28a54d5e6f3c91bcfac36e0dbfa
Ashish Karn <er.ashishkarn@gmail.com>
2022-09-03 19:06:15 +0530

    Deploying Neighborly App - Database configuration and vitual env dependency setup.

--- deploying-the-neighborly-app-with-azure-functions/NeighborlyAPI/deleteAdvertisement/__init__.py
+++ deploying-the-neighborly-app-with-azure-functions/NeighborlyAPI/deleteAdvertisement/__init__.py
@@ -9,9 +9,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if id:
         try:
-            url = "localhost"  # TODO: Update with appropriate MongoDB connection information
+            url = "mongodb://neighborly-app-cosmo-db-acc:YYF3vQcAVjA9uReaAL1rTaPZU2EpKizuoU2TbOD0R30W8aIAYgMbQEF0puWTLvTMpLp9chAAGtCHeBtTatRMaw==@neighborly-app-cosmo-db-acc.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@neighborly-app-cosmo-db-acc@"  # TODO: Update with appropriate MongoDB connection information
             client = pymongo.MongoClient(url)
-            database = client['azure']
+            database = client['neighborly-mongo-db']
             collection = database['advertisements']
             
             query = {'_id': ObjectId(id)}
