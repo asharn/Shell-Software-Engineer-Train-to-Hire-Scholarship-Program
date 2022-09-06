905b7d728a381dfb4952a9cf007d5edfd85a9ad2
Ashish Karn <er.ashishkarn@gmail.com>
2022-09-06 21:46:40 +0530

    Practice Cloud Computing : Application Logging excersise completed.

--- practice-cloud-computing/endpoints-for-application-status/exercises/python-helloworld/app.py
+++ practice-cloud-computing/endpoints-for-application-status/exercises/python-helloworld/app.py
@@ -1,13 +1,19 @@
 from flask import Flask
 import json
+import logging
+
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    app.logger.info('Main request successfull')
     return "Hello World!"
 
 @app.route('/status')
 def status():
+    app.logger.info('/status service is call with response : %s',{"result":"OK - healthy"})
     response = app.response_class(
             response=json.dumps({"result":"OK - healthy"}),
             status=200,
@@ -18,6 +24,7 @@ def status():
 
 @app.route('/metrics')
 def metrics():
+    app.logger.info('/status service is call with response : ',{"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}})
     response = app.response_class(
             response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
             status=200,
@@ -28,4 +35,5 @@ def metrics():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
