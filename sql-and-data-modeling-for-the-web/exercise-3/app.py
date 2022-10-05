11024265e88fdab41971e21fb816713378d5b945
Ashish Karn <er.ashishkarn@gmail.com>
2022-10-05 22:15:05 +0530

    SQL and Data Modeling: Exercise 3rd completed.

--- sql-and-data-modeling-for-the-web/exercise-3/app.py
+++ sql-and-data-modeling-for-the-web/exercise-3/app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/example'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:gameover@localhost:5432/example'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -13,9 +13,15 @@ class Person(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(), nullable=False)
 
-db.create_all()
+with app.app_context():
+        db.create_all()
 
 @app.route('/')
-
 def index():
   return 'Hello World!'
+
+
+#This code goes at the bottom of your flask Python file(this is actually your server)
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=3000)
